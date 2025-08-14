7276cf7243d2c0264a6fda4c26225f16ea45a220
soham901 <sagathiyasoham12345@gmail.com>
2025-08-14 12:46:10 +0530

    feat: initial commit of Next.js app
    - Added Next.js app structure
    - Created basic page component
    - Implemented UI button component
    - Set up Tailwind CSS and styling
    - Added basic .gitignore and README

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  );
+}
